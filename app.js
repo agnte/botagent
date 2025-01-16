26b4be07bb94488942c1cb62969c7f70b9ba5192
rido-min <rido-min@users.noreply.github.com>
2025-01-15 20:27:51 -0800

    style: format code for consistency and readability

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ const server = express()
 server.use(authorizeJWT(config))
 server.use(json())
 
-server.post('/api/messages', 
-    async (req, res) => {
-        await adapter.process(req, res, (context) => myBot.run(context));
-    }
+server.post('/api/messages',
+  async (req, res) => {
+    await adapter.process(req, res, (context) => myBot.run(context));
+  }
 )
 
 const port = process.env.PORT || 3978
 
 server.listen(port, () => {
-    console.log(`\n lisenting on ${ port } for bot ${ process.env.clientId }`);
+  console.log(`\n lisenting on ${port} for bot ${process.env.clientId}`);
 })
--- bot.js
+++ bot.js
@@ -4,28 +4,29 @@ import { ActivityHandler, MessageFactory } from '@microsoft/agents-bot-hosting';
 let counter = 0
 
 export class EchoBot extends ActivityHandler {
-    constructor() {
-        super()
-        this.onMessage(async (context, next) => {
-            const replyText = `Echo ${counter++}: ${ context.activity.text } 🤖`
-            await context.sendActivity(MessageFactory.text(replyText, replyText))
-            await next()
-        });
+  constructor() {
+    super()
+    this.onMessage(async (context, next) => {
+      const replyText = `Echo ${counter++}: ${context.activity.text}`
+      await context.sendActivity(MessageFactory.text(replyText, replyText))
+      await next()
+    });
 
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded
-            const welcomeText = 'Hello from the Agents SDK and welcome!'
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded
+      const welcomeText = 'Hello from the Agents SDK and welcome!'
 
-            if (membersAdded === undefined) {
-                return await next()
-            }
-            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-                await context.sendActivity(MessageFactory.text(membersAdded[cnt].id))
-                if (membersAdded[cnt].id !== context.activity.recipient?.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText))
-                }
-            }
-            await next()
-        })
-    }
+      if (membersAdded === undefined) {
+        return await next()
+      }
+
+      for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+        await context.sendActivity(MessageFactory.text(membersAdded[cnt].id))
+        if (membersAdded[cnt].id !== context.activity.recipient?.id) {
+          await context.sendActivity(MessageFactory.text(welcomeText, welcomeText))
+        }
+      }
+      await next()
+    })
+  }
 }
